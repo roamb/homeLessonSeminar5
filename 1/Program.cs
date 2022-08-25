4fa008edbf8f797c3e5139f48368347c32759aaa
roan b <roman7tati@gmail.com>
2022-08-25 21:34:51 +0300

    first commit

---
+++ 1/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] Array(int size, int min, int max){
+    int[] result = new int[size];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(100, 999);
+        Console.Write($"{result[i]} ");
+    }
+    return result;
+}
+int[] array = Array(10, 100, 999);
+Console.WriteLine();
+int sum = 0;
+for (int i = 0; i < array.Length; i++){
+    if (array[i] % 2 == 0)
+    {
+        sum += 1;
+    }
+}
+Console.WriteLine(sum);
---
+++ 2/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] Array(int size, int min, int max)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(1, 99);
+        Console.Write($"{result[i]} ");
+    }
+    return result;
+}
+int[] array = Array(6, 1, 99);
+Console.WriteLine();
+int sum = 0;
+for (int i=0; i<array.Length; i++){
+    if (array[i]%2 != 0){
+        sum = sum + array[i];
+    }
+}
+Console.WriteLine(sum);
